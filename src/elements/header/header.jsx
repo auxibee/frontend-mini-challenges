e0a1b38f8953c968cec10f18595de3d941225746
auxibee <auxibee@gmail.com>
2022-12-30 06:35:36 +0000

    removed green color on links hover

--- src/elements/header/header.jsx
+++ src/elements/header/header.jsx
@@ -5,11 +5,9 @@ const Header = ({ title, showFullPage = true }) => {
   return (
     <header>
       <div className="header-wrapper">
+        <nav>{showFullPage && <Link to="/">Home</Link>}</nav>
         <h1>{title}</h1>
-        <nav>
-          {showFullPage && <Link to="/">Home</Link>}
-          {showFullPage && <Link to="/">GitHub</Link>}
-        </nav>
+        <nav>{showFullPage && <Link to="/">GitHub</Link>}</nav>
       </div>
     </header>
   );

a8a868dfb54875eb3284ec81581def49a4777310
auxibee <auxibee@gmail.com>
2022-12-31 08:42:00 +0000

    added state for the telephoneFormatter component

--- src/telephone-formatter/telephoneformatter.jsx
+++ src/telephone-formatter/telephoneformatter.jsx
@@ -1,14 +1,25 @@
 import Input from "../elements/input";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "../elements/header/header";
 import Main from "../elements/main/main";
 
 import "./telephoneformatter.css";
 
+const formatTelephone = (number) => {
+  const subStr = number.slice(0, 3);
+
+  if (subStr.length === 3 && subStr[0] !== "+") {
+    return `+(${subStr}) - `;
+  } else {
+    return number;
+  }
+};
+
 const TelephoneFormatter = () => {
   const [telephone, setTelephone] = useState("");
+
   const handleTelephoneFormat = (e) => {
-    setTelephone(e.target.value);
+    setTelephone(() => formatTelephone(e.target.value));
   };
   return (
     <>
